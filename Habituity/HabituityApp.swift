3acd0f29f59fc58acae3e64fce915e26a5023d4d
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-11-10 05:35:37 -0800

    Initial Commit

---
+++ Habituity/HabituityApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabituityApp.swift
+//  Habituity
+//
+//  Created by Luis Cardenas on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HabituityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
